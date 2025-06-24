99ae38f5d7a44d87197078ec662bb9cc3f2ee99d
mikasagi <mikasagi.wug@gmail.com>
2025-06-24 19:47:18 +0900

    Updates

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
-  assetPrefix: process.env.ASSET_PREFIX,
-};
-
-export default nextConfig;
